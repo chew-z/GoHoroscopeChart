ea09dd5a859923c71d9867efc991bedcae14bc1a
Robert J <chew-z@users.noreply.github.com>
2022-04-25 23:27:49 +0200

    Static files rtc.

--- main.go
+++ main.go
@@ -26,20 +26,14 @@ func main() {
 	defer swephgo.Close()
 	//
 	router := gin.Default()
+	router.SetTrustedProxies(nil)
 	// Process the templates at the start so that they don't have to be loaded
 	// from the disk again. This makes serving HTML pages very fast.
 	router.LoadHTMLGlob("templates/*")
-	router.Static("/static", "./static")
 	router.StaticFile("/favicon.ico", "./static/favicon.ico") // some clients don't read webmanifest
 	router.StaticFile("/astrochart.min.js", "./static/astrochart.min.js")
 	router.GET("/", PrintHoroscope)
-	router.GET("/animate", printTransit)
 	router.GET("/radix", printRadix)
 	router.GET("/transit", printTransit)
-	router.GET("/ping", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
-	router.Run()
+	router.Run(":8080")
 }
